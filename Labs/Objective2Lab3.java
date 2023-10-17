692df73b65dd1a42f9560e8b9220f134ccdd0a7f
Zach Nyman <zachery.nyman@gmail.com>
2023-10-17 13:44:53 -0600

    Adding completed labs.

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int yearBorn = 1993;
+    String monthBorn = "July";
+    boolean isWeekend = true;
+
+    System.out.println(yearBorn + "\n" + monthBorn + "\n" + isWeekend);
+
+  }
+}
---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+	public static void main(String args []) {
+		double side1 = 10, side2 = 8, hypotenuse;
+		
+		hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+		
+		System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is "
+			+ hypotenuse);
+
+	}
+}
